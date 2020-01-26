2e9551bcbad553d8105bef65db138c20bebe0aa2
Uwais Iqbal <uwais.iqbal@thomsonreuters.com>
2020-01-26 15:50:11 +0000

    small fix

--- app.py
+++ app.py
@@ -61,7 +61,7 @@ def get_verse(surah: int, verse: int):
         )
         i += 1
 
-    r = trans_json['verses'][verse]
+    r = trans_json['verses'][verse + 1]
     r['translation'] = verses[verse]['translation']
     return r
 
